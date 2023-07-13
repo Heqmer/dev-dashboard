a8ddb5937b37fc1673af9a9f910472d0aaf65e72
Heqmer <damian.m1991@gmail.com>
2023-07-13 18:55:06 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import './App.css';
-import logo from './logo192.png'
-import {Col, Container, Row} from "react-bootstrap";
+import {Col} from "react-bootstrap";
 import React from "react";
 import {CounterButton} from "./components/CounterButton";
 import {Grid} from "./components/grid/Grid";
-import FigureImage from "react-bootstrap/FigureImage";
 
 function App() {
   return (
-    <Container fluid>
-      <Row className={"vh-100"}>
-        <Col sm={2} className={"Sidebar p-4 d-flex flex-column"}>
-          <div className={"d-grid gap-3 shadow-sm Green"}>
-            <h1 className={"display-6 text-center text-warning "}>Dev Dashboard</h1>
-            <hr className={"hr border border-warning"}></hr>
+    <main className={"d-flex "}>
+      <div className={"d-flex flex-column flex-shrink-0 p-3 bg-dark"} style={{width: 300}}>
+        {/*<h1 className={"text-center text-warning "}>Dev Dashboard</h1>*/}
+        {/*<hr className={"hr border border-warning"}></hr>*/}
+        <ul className={"nav nav-pills flex-column mb-auto"}>
+          <li>
             <button type={"button"} className={"rounded-4 btn btn-outline-warning"}>Home</button>
+          </li>
+          <li>
             <button type={"button"} className={"rounded-4 btn btn-outline-warning"}>Archive</button>
+          </li>
+          <li>
             <button type={"button"} className={"rounded-4 btn btn-outline-warning"}>Contact</button>
-          </div>
-          <div className={"Red mt-auto"}>
-            <hr className={"hr border border-warning"}></hr>
-            <FigureImage alt={"Dev Dashboard"} src={logo} className={""}></FigureImage>
-          </div>
-        </Col>
-        <Col sm={10} className={"MainPanel p-4"}>
-          <header>
-            <p>
-              Nested component test:
-            </p>
-            <p>
-              <CounterButton/>
-            </p>
-            <Grid/>
-          </header>
-        </Col>
-      </Row>
-    </Container>
+          </li>
+        </ul>
+        <hr className={"hr border border-warning"}></hr>
+        {/*<Image alt={"Dev Dashboard"} src={logo} className={""}></Image>*/}
+      </div>
+      <Col sm={10} className={"MainPanel p-4"}>
+        <header>
+          <p>
+            Nested component test:
+          </p>
+          <p>
+            <CounterButton/>
+          </p>
+          <Grid/>
+        </header>
+      </Col>
+    </main>
   );
 }
 
